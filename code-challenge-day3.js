ec346a4c54acbf0bb84ccb9ec51bd8222f4e9702
Miguel Camilo <miguelcamilo48@gmail.com>
2023-01-14 15:44:55 -0500

    added a coding challenge from another file

--- code-challenge-day3.js
+++ code-challenge-day3.js
@@ -1,4 +1,4 @@
-//solving using with out Math.max()
+//! solving using with out Math.max()
 const array = [
 	100,
 	200,
@@ -12,9 +12,9 @@ const array = [
 function grabNumber(arr) {
 	let largeNumber = 0;
 
-	//! this runs through the entire array and it will detect each num
-	//! and it will check it against the conditional if the conditional is true
-	//! then exit and return that value
+	// this runs through the entire array and it will detect each num
+	// and it will check it against the conditional if the conditional is true
+	// then exit and return that value
 	for (let i = 0; i < arr.length; i++) { 
 
 		if (arr[i] > largeNumber) {
@@ -35,3 +35,38 @@ function grabNumber(arr) {
 	return largest;
 }
 console.log(grabNumber(array));
+
+
+
+/** 
+//! Given any array of numbers (all will be > 0),
+//! write a function that returns the number of odd
+//!  numbers in the array.
+*/
+
+const myArray = [
+    17,
+    24, 
+    69, 
+    86, 
+    42, 
+    13, 
+    11
+];
+
+function findOdd(list) {
+
+    let newArray = [];
+    
+    // this will run through the array.length
+	for (let i = 0; i < list.length; i++) { 
+        // checks each element in list to see if it returns 1 and if true pushes to new newArray
+        if (list[i] % 2 === 1) {
+            newArray.push(list[i])
+		}
+	}
+	return newArray.length;
+}
+
+const numOdd = findOdd(myArray); // should return 4
+console.log(`The amount of odd numbers in the array are: ${numOdd}`);
