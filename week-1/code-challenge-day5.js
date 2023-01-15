a86f7e43e0ee9b9706d0c5a0a644819c6fa6474c
Miguel Camilo <miguelcamilo48@gmail.com>
2023-01-15 09:46:34 -0500

    uncommented the code blocks

--- week-1/code-challenge-day5.js
+++ week-1/code-challenge-day5.js
@@ -1,26 +1,36 @@
 //! 1. Write a temp converter, Given a temp in celcius. Output the equivalent temp in Farenheit
 
-// const tempConvert = celsius => { // func uses celcius to fahren formula 
-//     const fahren = (celsius * 1.8) + 32
-//     return fahren
-// }
+const tempConvert = celsius => { // func uses celcius to fahren formula 
+    const fahren = (celsius * 1.8) + 32
+    return fahren
+}
 
-// const input = 29
-// const convertTemp = tempConvert(input) // takes input = 29 and passes it to the func then the func returns the value and stores it in the convertTemp var
+const input = 29
+const convertTemp = tempConvert(input) // takes input = 29 and passes it to the func then the func returns the value and stores it in the convertTemp var
 
-// console.log(`${input} degrees Celsius is ${convertTemp} degrees Fahrenheit.`)
+console.log(`${input} degrees Celsius is ${convertTemp} degrees Fahrenheit.`)
+
+
+/// / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / 
+
 
 //! 2. Convert one currency to USD
-// const roundNumber = num => Math.round(num * 100) / 100 // function created to round
+const roundNumber = num => Math.round(num * 100) / 100 // function created to round
 
-// const currencyConverter = yen => { // func uses yen to usd formula
-//     const usd = (yen * .0078)
-//     return usd
-// }
-// const yen = 1
-// const convertedCurrency = currencyConverter(yen)
+const currencyConverter = yen => { // func uses yen to usd formula
+    const usd = (yen * .0078)
+    return usd
+}
+const yen = 1
+const convertedCurrency = currencyConverter(yen)
 
-// console.log(`${yen} yen is $${roundNumber(convertedCurrency)}`)
+console.log(`${yen} yen is $${roundNumber(convertedCurrency)}`)
+
+
+
+/// / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / 
+
+
 
 //! 3. Given a num of rows and colums, ouput a grid of *'s
 function cloudPyramid(row, column) {
